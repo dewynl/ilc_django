ed43bd1448d9537732e57bfc937fdf7368a4d24a
dewyn <dewyn.liriano@gmail.com>
2018-12-17 14:52:54 -0400

    cambiando Procfile a gunicorn.

--- ilc/settings.py
+++ ilc/settings.py
@@ -1,3 +1,4 @@
+import django_heroku
 """
 Django settings for ilc project.
 
@@ -127,3 +128,7 @@ STATIC_DIRS = 'static'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
+
